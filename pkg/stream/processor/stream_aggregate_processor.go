e7f266cb02b4c7c7941550b8e647741e33ce35d8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-09-15 01:44:19 -0500

    checkpoint
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/stream/processor/stream_aggregate_processor.go
+++ pkg/stream/processor/stream_aggregate_processor.go
@@ -1,5 +1,21 @@
 package processor
 
 type StreamAggregateProcessor struct {
+	pipe  Pipe
 	store KeyValueStore
+	pctx  ProcessorContext
+}
+
+var _ = Processor(&StreamAggregateProcessor{})
+
+func (p *StreamAggregateProcessor) WithPipe(pipe Pipe) {
+	p.pipe = pipe
+}
+
+func (p *StreamAggregateProcessor) WithProcessorContext(pctx ProcessorContext) {
+	p.pctx = pctx
+}
+
+func (p *StreamAggregateProcessor) Process(Message) error {
+	panic("not implemented")
 }
