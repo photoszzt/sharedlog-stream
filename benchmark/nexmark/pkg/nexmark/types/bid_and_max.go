c953ebbb92980915f34e3e01c3d6336274a1c2c9
zhiting zhu <zhitingz@cs.utexas.edu>
2021-10-22 16:50:42 -0500

    add BidAndMax and PriceTime
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

---
+++ benchmark/nexmark/pkg/nexmark/types/bid_and_max.go
@@ -0,0 +1,44 @@
+//go:generate msgp
+//msgp:ignore BidAndMaxJSONSerde BidAndMaxMsgpSerde
+package types
+
+import "encoding/json"
+
+type BidAndMax struct {
+	Price       uint64 `json:"price" msg:"price"`
+	Auction     uint64 `json:"auction" msg:"auction"`
+	Bidder      uint64 `json:"bidder" msg:"bidder"`
+	DateTime    int64  `json:"dateTime" msg:"dateTime"`
+	MaxDateTime int64  `json:"maxDateTime" msg:"maxDateTime"`
+	Extra       string `json:"extra" msg:"extra"`
+}
+
+type BidAndMaxJSONSerde struct{}
+
+func (s BidAndMaxJSONSerde) Encode(value interface{}) ([]byte, error) {
+	bm := value.(*BidAndMax)
+	return json.Marshal(bm)
+}
+
+func (s BidAndMaxJSONSerde) Decode(value []byte) (interface{}, error) {
+	bm := BidAndMax{}
+	if err := json.Unmarshal(value, &bm); err != nil {
+		return nil, err
+	}
+	return bm, nil
+}
+
+type BidAndMaxMsgpSerde struct{}
+
+func (s BidAndMaxMsgpSerde) Encode(value interface{}) ([]byte, error) {
+	bm := value.(*BidAndMax)
+	return bm.MarshalMsg(nil)
+}
+
+func (s BidAndMaxMsgpSerde) Decode(value []byte) (interface{}, error) {
+	bm := BidAndMax{}
+	if _, err := bm.UnmarshalMsg(value); err != nil {
+		return nil, err
+	}
+	return bm, nil
+}
---
+++ benchmark/nexmark/pkg/nexmark/types/price_time.go
@@ -0,0 +1,45 @@
+//go:generate msgp
+//msgp:ignore PriceTimeJSONSerde PriceTimeMsgpSerde
+package types
+
+import (
+	"encoding/json"
+	"sharedlog-stream/pkg/stream/processor"
+)
+
+type PriceTime struct {
+	Price    uint64 `json:"price" msg:"price"`
+	dateTime int64  `json:"dateTime" msg:"dateTime"` // unix timestamp in ms
+}
+
+type PriceTimeJSONSerde struct{}
+
+var _ = processor.Encoder(PriceTimeJSONSerde{})
+
+func (s PriceTimeJSONSerde) Encode(value interface{}) ([]byte, error) {
+	pt := value.(*PriceTime)
+	return json.Marshal(pt)
+}
+
+func (s PriceTimeJSONSerde) Decode(value []byte) (interface{}, error) {
+	pt := PriceTime{}
+	if err := json.Unmarshal(value, &pt); err != nil {
+		return nil, err
+	}
+	return pt, nil
+}
+
+type PriceTimeMsgpSerde struct{}
+
+func (s PriceTimeMsgpSerde) Encode(value interface{}) ([]byte, error) {
+	pt := value.(*PriceTime)
+	return pt.MarshalMsg(nil)
+}
+
+func (s PriceTimeMsgpSerde) Decode(value []byte) (interface{}, error) {
+	pt := PriceTime{}
+	if _, err := pt.UnmarshalMsg(value); err != nil {
+		return nil, err
+	}
+	return pt, nil
+}
