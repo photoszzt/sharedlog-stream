7e8c9ac7f09b82ddcd74d6e3dc666f6fdd9ff16b
zhiting zhu <zhitingz@cs.utexas.edu>
2021-10-22 17:35:36 -0500

    backup repartition for q7
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

---
+++ benchmark/nexmark/pkg/nexmark/handlers/q7_bidKeyedByPrice.go
@@ -0,0 +1,143 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sharedlog-stream/benchmark/common"
+	ntypes "sharedlog-stream/benchmark/nexmark/pkg/nexmark/types"
+	"sharedlog-stream/benchmark/nexmark/pkg/nexmark/utils"
+	"sharedlog-stream/pkg/sharedlog_stream"
+	"sharedlog-stream/pkg/stream/processor"
+	"time"
+
+	"cs.utexas.edu/zjia/faas/types"
+)
+
+type q7BidKeyedByPrice struct {
+	env types.Environment
+}
+
+func NewQ7BidKeyedByPriceHandler(env types.Environment) types.FuncHandler {
+	return &q7BidKeyedByPrice{
+		env: env,
+	}
+}
+
+func (h *q7BidKeyedByPrice) Call(ctx context.Context, input []byte) ([]byte, error) {
+	parsedInput := &common.QueryInput{}
+	err := json.Unmarshal(input, parsedInput)
+	if err != nil {
+		return nil, err
+	}
+	output := h.process(ctx, parsedInput)
+	encodedOutput, err := json.Marshal(output)
+	if err != nil {
+		panic(err)
+	}
+	return utils.CompressData(encodedOutput), nil
+}
+
+func (h *q7BidKeyedByPrice) process(ctx context.Context, input *common.QueryInput) *common.FnOutput {
+	inputStream, err := sharedlog_stream.NewShardedSharedLogStream(ctx, h.env, input.InputTopicName, uint8(input.NumInPartition))
+	if err != nil {
+		return &common.FnOutput{
+			Success: false,
+			Message: fmt.Sprintf("NewSharedlogStream for input stream failed: %v", err),
+		}
+	}
+
+	outputStream, err := sharedlog_stream.NewShardedSharedLogStream(ctx, h.env, input.OutputTopicName, uint8(input.NumOutPartition))
+	if err != nil {
+		return &common.FnOutput{
+			Success: false,
+			Message: fmt.Sprintf("NewSharedlogStream for output stream failed: %v", err),
+		}
+	}
+
+	msgSerde, err := processor.GetMsgSerde(input.SerdeFormat)
+	if err != nil {
+		return &common.FnOutput{
+			Success: false,
+			Message: err.Error(),
+		}
+	}
+	eventSerde, err := getEventSerde(input.SerdeFormat)
+	if err != nil {
+		return &common.FnOutput{
+			Success: false,
+			Message: err.Error(),
+		}
+	}
+	inConfig := &sharedlog_stream.SharedLogStreamConfig{
+		Timeout:      time.Duration(input.Duration) * time.Second,
+		KeyDecoder:   processor.StringDecoder{},
+		ValueDecoder: eventSerde,
+		MsgDecoder:   msgSerde,
+	}
+	outConfig := &sharedlog_stream.StreamSinkConfig{
+		MsgEncoder:   msgSerde,
+		KeyEncoder:   processor.Uint64Encoder{},
+		ValueEncoder: eventSerde,
+	}
+
+	duration := time.Duration(input.Duration) * time.Second
+	src := sharedlog_stream.NewShardedSharedLogStreamSource(inputStream, inConfig)
+	sink := sharedlog_stream.NewShardedSharedLogStreamSink(outputStream, outConfig)
+
+	bid := processor.NewMeteredProcessor(processor.NewStreamFilterProcessor(processor.PredicateFunc(func(msg *processor.Message) (bool, error) {
+		event := msg.Value.(ntypes.Event)
+		return event.Etype == ntypes.BID, nil
+	})))
+	bidKeyedByPrice := processor.NewMeteredProcessor(processor.NewStreamMapProcessor(processor.MapperFunc(func(msg processor.Message) (processor.Message, error) {
+		event := msg.Value.(*ntypes.Event)
+		return processor.Message{Key: event.Bid.Price, Value: msg.Value, Timestamp: msg.Timestamp}, nil
+	})))
+	latencies := make([]int, 0, 128)
+	startTime := time.Now()
+	for {
+		if duration != 0 && time.Since(startTime) >= duration {
+			break
+		}
+		procStart := time.Now()
+		msg, err := src.Consume(input.ParNum)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: err.Error(),
+			}
+		}
+		bidMsg, err := bid.ProcessAndReturn(msg)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("filter bid err: %v", err),
+			}
+		}
+		if bidMsg != nil {
+			mappedKey, err := bidKeyedByPrice.ProcessAndReturn(bidMsg[0])
+			if err != nil {
+				return &common.FnOutput{
+					Success: false,
+					Message: fmt.Sprintf("bid keyed by price error: %v\n", err),
+				}
+			}
+			key := mappedKey[0].Key.(uint64)
+			par := uint8(key % uint64(input.NumOutPartition))
+			err = sink.Sink(mappedKey[0], par)
+			if err != nil {
+				return &common.FnOutput{
+					Success: false,
+					Message: fmt.Sprintf("sink err: %v\n", err),
+				}
+			}
+		}
+		elapsed := time.Since(procStart)
+		latencies = append(latencies, int(elapsed.Microseconds()))
+	}
+	return &common.FnOutput{
+		Success:   true,
+		Duration:  time.Since(startTime).Seconds(),
+		Latencies: map[string][]int{"e2e": latencies},
+	}
+}
