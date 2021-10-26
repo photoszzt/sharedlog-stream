de3c96d2a77e946b1579597b74ae903fa2210020
zhiting zhu <zhitingz@cs.utexas.edu>
2021-10-26 11:13:05 -0500

    fix print
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- benchmark/common/tput_lat.go
+++ benchmark/common/tput_lat.go
@@ -7,15 +7,16 @@ import (
 
 func ProcessThroughputLat(name string, latencies map[string][]int, duration float64) {
 
-	for name, lat_arr := range latencies {
+	for n, lat_arr := range latencies {
 		sumTime := float64(0)
 		for _, lat := range lat_arr {
 			sumTime += float64(lat) / 1000.0
 		}
 		sumTime = sumTime / 1000.0 // convert to sec
 		tput := float64(len(lat_arr)) / sumTime
-		fmt.Fprintf(os.Stdout, "sum of %s time: %v ", name, sumTime)
+		fmt.Fprintf(os.Stdout, "sum of %s time: %v ", n, sumTime)
 		fmt.Fprintf(os.Stdout, "processed: %v, throughput: (event/s) %v\n", len(lat_arr), tput)
 	}
-	fmt.Fprintf(os.Stdout, "%s duration: %v ", name, duration)
+	fmt.Fprintf(os.Stdout, "%s duration: %v\n", name, duration)
+	fmt.Fprintf(os.Stderr, "\n")
 }
