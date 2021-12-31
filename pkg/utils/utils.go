b9859f55d4e746714c941ffeadf409f6832f88d9
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-12-30 22:57:04 -0600

    refactor debug
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
 func MaxUint64(a, b uint64) uint64 {
 	if a > b {
 		return a
@@ -29,5 +35,70 @@ func MinUint64(a, b uint64) uint64 {
 		return a
 	} else {
 		return b
+	}
+}
+
+func GetStringValue(v interface{}) string {
+	if v == nil {
+		return ""
+	}
+
+	switch a := v.(type) {
+	case func() string:
+		return a()
+
+	case string:
+		return a
+
+	case fmt.Stringer:
+		return a.String()
+	}
+
+	val := reflect.ValueOf(v)
+	if val.Kind() == reflect.Ptr && !val.IsNil() {
+		val = val.Elem()
+	}
+
+	return reprOfValue(val)
+}
+
+func reprOfValue(val reflect.Value) string {
+	switch vt := val.Interface().(type) {
+	case bool:
+		return strconv.FormatBool(vt)
+	case error:
+		return vt.Error()
+	case float32:
+		return strconv.FormatFloat(float64(vt), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(vt, 'f', -1, 64)
+	case fmt.Stringer:
+		return vt.String()
+	case int:
+		return strconv.Itoa(vt)
+	case int8:
+		return strconv.Itoa(int(vt))
+	case int16:
+		return strconv.Itoa(int(vt))
+	case int32:
+		return strconv.Itoa(int(vt))
+	case int64:
+		return strconv.FormatInt(vt, 10)
+	case string:
+		return vt
+	case uint:
+		return strconv.FormatUint(uint64(vt), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(vt), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(vt), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(vt), 10)
+	case uint64:
+		return strconv.FormatUint(vt, 10)
+	case []byte:
+		return string(vt)
+	default:
+		return fmt.Sprint(val.Interface())
 	}
 }
