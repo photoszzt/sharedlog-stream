b52cf0edbecbd4603e2fcc979c3dbc1b5944d462
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-01-18 01:35:36 -0600

    backup
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- benchmark/nexmark/pkg/nexmark/handlers/q3_jointable.go
+++ benchmark/nexmark/pkg/nexmark/handlers/q3_jointable.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"sharedlog-stream/benchmark/common"
+	ntypes "sharedlog-stream/benchmark/nexmark/pkg/nexmark/types"
 	"sharedlog-stream/benchmark/nexmark/pkg/nexmark/utils"
+	"sharedlog-stream/pkg/sharedlog_stream"
 	"sharedlog-stream/pkg/stream/processor"
 	"sharedlog-stream/pkg/stream/processor/commtypes"
 	"sharedlog-stream/pkg/stream/processor/store"
@@ -14,19 +16,19 @@ import (
 	"cs.utexas.edu/zjia/faas/types"
 )
 
-type query3JoinTableProcessorHandler struct {
+type q3JoinTableHandler struct {
 	env           types.Environment
 	currentOffset map[string]uint64
 }
 
-func NewQuery3JoinTableProcessor(env types.Environment) types.FuncHandler {
-	return &query3JoinTableProcessorHandler{
+func NewQ3JoinTableHandler(env types.Environment) types.FuncHandler {
+	return &q3JoinTableHandler{
 		env:           env,
 		currentOffset: make(map[string]uint64),
 	}
 }
 
-func (h *query3JoinTableProcessorHandler) Call(ctx context.Context, input []byte) ([]byte, error) {
+func (h *q3JoinTableHandler) Call(ctx context.Context, input []byte) ([]byte, error) {
 	parsedInput := &common.QueryInput{}
 	err := json.Unmarshal(input, parsedInput)
 	if err != nil {
@@ -41,18 +43,18 @@ func (h *query3JoinTableProcessorHandler) Call(ctx context.Context, input []byte
 	return utils.CompressData(encodedOutput), nil
 }
 
-func (h *query3JoinTableProcessorHandler) process(ctx context.Context,
+func (h *q3JoinTableHandler) process(ctx context.Context,
 	argsTmp interface{},
 	trackParFunc func([]uint8) error,
 ) (map[string]uint64, *common.FnOutput) {
 	return h.currentOffset, nil
 }
 
-func (h *query3JoinTableProcessorHandler) toAuctionsBySellerIDTable(
+func (h *q3JoinTableHandler) toAuctionsBySellerIDTable(
 	sp *common.QueryInput,
 	eventSerde commtypes.Serde,
 	msgSerde commtypes.MsgSerde,
-) (*processor.MeteredProcessor, error) {
+) (*processor.MeteredProcessor, *store.InMemoryKeyValueStore, error) {
 	auctionsBySellerIDStoreName := "auctionsBySellerIDStore"
 	auctionsBySellerIDStore := store.NewInMemoryKeyValueStore(auctionsBySellerIDStoreName, func(a, b treemap.Key) int {
 		valA := a.(uint64)
@@ -66,14 +68,14 @@ func (h *query3JoinTableProcessorHandler) toAuctionsBySellerIDTable(
 		}
 	})
 	toTableProc := processor.NewMeteredProcessor(processor.NewStoreToKVTableProcessor(auctionsBySellerIDStore))
-	return toTableProc, nil
+	return toTableProc, auctionsBySellerIDStore, nil
 }
 
-func (h *query3JoinTableProcessorHandler) toPersonsByIDMapTable(
+func (h *q3JoinTableHandler) toPersonsByIDMapTable(
 	sp *common.QueryInput,
 	eventSerde commtypes.Serde,
 	msgSerde commtypes.MsgSerde,
-) (*processor.MeteredProcessor, error) {
+) (*processor.MeteredProcessor, *store.InMemoryKeyValueStore, error) {
 	personsByIDStoreName := "personsByIDStore"
 	personsByIDStore := store.NewInMemoryKeyValueStore(personsByIDStoreName, func(a, b treemap.Key) int {
 		valA := a.(uint64)
@@ -87,13 +89,126 @@ func (h *query3JoinTableProcessorHandler) toPersonsByIDMapTable(
 		}
 	})
 	toTableProc := processor.NewMeteredProcessor(processor.NewStoreToKVTableProcessor(personsByIDStore))
-	return toTableProc, nil
+	return toTableProc, personsByIDStore, nil
 }
 
-func (h *query3JoinTableProcessorHandler) Query3JoinTable(ctx context.Context, sp *common.QueryInput) *common.FnOutput {
+func (h *q3JoinTableHandler) getShardedInputOutputStreams(ctx context.Context,
+	input *common.QueryInput,
+) (*sharedlog_stream.ShardedSharedLogStream, /* auction */
+	*sharedlog_stream.ShardedSharedLogStream, /* person */
+	*sharedlog_stream.ShardedSharedLogStream, /* output */
+	error,
+) {
+	inputStreamAuction, err := sharedlog_stream.NewShardedSharedLogStream(h.env, input.InputTopicNames[0], uint8(input.NumInPartition))
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewSharedlogStream for input stream failed: %v", err)
 
+	}
+	inputStreamPerson, err := sharedlog_stream.NewShardedSharedLogStream(h.env, input.InputTopicNames[1], uint8(input.NumInPartition))
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewSharedlogStream for input stream failed: %v", err)
+
+	}
+	outputStream, err := sharedlog_stream.NewShardedSharedLogStream(h.env, input.OutputTopicName, uint8(input.NumOutPartition))
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewSharedlogStream for output stream failed: %v", err)
+	}
+	return inputStreamAuction, inputStreamPerson, outputStream, nil
+}
+
+func (h *q3JoinTableHandler) getSrcSink(ctx context.Context, sp *common.QueryInput,
+	auctionsStream *sharedlog_stream.ShardedSharedLogStream,
+	personsStream *sharedlog_stream.ShardedSharedLogStream,
+	outputStream *sharedlog_stream.ShardedSharedLogStream,
+	eventSerde commtypes.Serde,
+	msgSerde commtypes.MsgSerde,
+) (*processor.MeteredSource, /* auctionSrc */
+	*processor.MeteredSource, /* personsSrc */
+	*processor.MeteredSink, error,
+) {
+	auctionsConfig := &sharedlog_stream.SharedLogStreamConfig{
+		Timeout:      common.SrcConsumeTimeout,
+		KeyDecoder:   commtypes.Uint64Decoder{},
+		ValueDecoder: eventSerde,
+		MsgDecoder:   msgSerde,
+	}
+	personsConfig := &sharedlog_stream.SharedLogStreamConfig{
+		Timeout:      common.SrcConsumeTimeout,
+		KeyDecoder:   commtypes.Uint64Decoder{},
+		ValueDecoder: eventSerde,
+		MsgDecoder:   msgSerde,
+	}
+	outConfig := &sharedlog_stream.StreamSinkConfig{
+		ValueEncoder: commtypes.EncoderFunc(func(val interface{}) ([]byte, error) {
+			ret := val.(*ntypes.NameCityStateId)
+			if sp.SerdeFormat == uint8(commtypes.JSON) {
+				return json.Marshal(ret)
+			} else {
+				return ret.MarshalMsg(nil)
+			}
+		}),
+	}
+
+	auctionsSrc := processor.NewMeteredSource(sharedlog_stream.NewShardedSharedLogStreamSource(auctionsStream, auctionsConfig))
+	personsSrc := processor.NewMeteredSource(sharedlog_stream.NewShardedSharedLogStreamSource(personsStream, personsConfig))
+	sink := processor.NewMeteredSink(sharedlog_stream.NewShardedSharedLogStreamSink(outputStream, outConfig))
+	return auctionsSrc, personsSrc, sink, nil
+}
+
+type q3JoinTableProcessArgs struct {
+	personSrc  *processor.MeteredSource
+	auctionSrc *processor.MeteredSource
+	sink       *processor.MeteredSink
+}
+
+func (h *q3JoinTableHandler) Query3JoinTable(ctx context.Context, sp *common.QueryInput) *common.FnOutput {
 	/*
-		joiner := processor.ValueJoinerFunc(func(leftVal interface{}, rightVal interface{}) interface{} {
+		msgSerde, err := commtypes.GetMsgSerde(sp.SerdeFormat)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("get msg serde err: %v", err),
+			}
+		}
+		eventSerde, err := getEventSerde(sp.SerdeFormat)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("get event serde err: %v", err),
+			}
+		}
+		auctionsStream, personsStream, outputStream, err := h.getShardedInputOutputStreams(ctx, sp)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("get input output err: %v", err),
+			}
+		}
+		auctionsSrc, personsSrc, sink, err := h.getSrcSink(ctx, sp, auctionsStream, personsStream, outputStream, eventSerde, msgSerde)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("getSrcSink err: %v\n", err),
+			}
+		}
+
+		toAuctionsTable, auctionsStore, err := h.toAuctionsBySellerIDTable(sp, eventSerde, msgSerde)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("toAucTab err: %v\n", err),
+			}
+		}
+
+		toPersonsTable, personsStore, err := h.toPersonsByIDMapTable(sp, eventSerde, msgSerde)
+		if err != nil {
+			return &common.FnOutput{
+				Success: false,
+				Message: fmt.Sprintf("toPersonsTab err: %v\n", err),
+			}
+		}
+
+		joiner := processor.ValueJoinerWithKeyFunc(func(readOnlyKey interface{}, leftVal interface{}, rightVal interface{}) interface{} {
 			event := rightVal.(*ntypes.Event)
 			return &ntypes.NameCityStateId{
 				Name:  event.NewPerson.Name,
@@ -102,6 +217,12 @@ func (h *query3JoinTableProcessorHandler) Query3JoinTable(ctx context.Context, s
 				ID:    event.NewPerson.ID,
 			}
 		})
+
+		personJoinsAuctions := processor.NewMeteredProcessor(
+			processor.NewTableTableJoinProcessor(auctionsStore.Name(), auctionsStore, joiner))
+
+		auctionJoinsPersons := processor.NewMeteredProcessor(
+			processor.NewTableTableJoinProcessor(personsStore.Name(), personsStore, joiner))
 	*/
 	return nil
 }
