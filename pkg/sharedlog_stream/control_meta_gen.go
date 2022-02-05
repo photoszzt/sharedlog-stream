f5e0b64283969579bfe2c3d41256f46355f78fab
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-02-04 23:07:22 -0600

    update gen
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/sharedlog_stream/control_meta_gen.go
+++ pkg/sharedlog_stream/control_meta_gen.go
@@ -24,18 +24,18 @@ func (z *ControlMetadata) DecodeMsg(dc *msgp.Reader) (err error) {
 			return
 		}
 		switch msgp.UnsafeString(field) {
-		case "Stages":
+		case "Config":
 			var zb0002 uint32
 			zb0002, err = dc.ReadMapHeader()
 			if err != nil {
-				err = msgp.WrapError(err, "Stages")
+				err = msgp.WrapError(err, "Config")
 				return
 			}
-			if z.Stages == nil {
-				z.Stages = make(map[string]uint8, zb0002)
-			} else if len(z.Stages) > 0 {
-				for key := range z.Stages {
-					delete(z.Stages, key)
+			if z.Config == nil {
+				z.Config = make(map[string]uint8, zb0002)
+			} else if len(z.Config) > 0 {
+				for key := range z.Config {
+					delete(z.Config, key)
 				}
 			}
 			for zb0002 > 0 {
@@ -44,15 +44,21 @@ func (z *ControlMetadata) DecodeMsg(dc *msgp.Reader) (err error) {
 				var za0002 uint8
 				za0001, err = dc.ReadString()
 				if err != nil {
-					err = msgp.WrapError(err, "Stages")
+					err = msgp.WrapError(err, "Config")
 					return
 				}
 				za0002, err = dc.ReadUint8()
 				if err != nil {
-					err = msgp.WrapError(err, "Stages", za0001)
+					err = msgp.WrapError(err, "Config", za0001)
 					return
 				}
-				z.Stages[za0001] = za0002
+				z.Config[za0001] = za0002
+			}
+		case "Topic":
+			z.Topic, err = dc.ReadString()
+			if err != nil {
+				err = msgp.WrapError(err, "Topic")
+				return
 			}
 		case "Key":
 			z.Key, err = dc.ReadBytes(z.Key)
@@ -66,10 +72,10 @@ func (z *ControlMetadata) DecodeMsg(dc *msgp.Reader) (err error) {
 				err = msgp.WrapError(err, "Epoch")
 				return
 			}
-		case "InstanceId":
-			z.InstanceId, err = dc.ReadUint8()
+		case "SubstreamId":
+			z.SubstreamId, err = dc.ReadUint8()
 			if err != nil {
-				err = msgp.WrapError(err, "InstanceId")
+				err = msgp.WrapError(err, "SubstreamId")
 				return
 			}
 		default:
@@ -85,28 +91,38 @@ func (z *ControlMetadata) DecodeMsg(dc *msgp.Reader) (err error) {
 
 // EncodeMsg implements msgp.Encodable
 func (z *ControlMetadata) EncodeMsg(en *msgp.Writer) (err error) {
-	// map header, size 4
-	// write "Stages"
-	err = en.Append(0x84, 0xa6, 0x53, 0x74, 0x61, 0x67, 0x65, 0x73)
+	// map header, size 5
+	// write "Config"
+	err = en.Append(0x85, 0xa6, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67)
 	if err != nil {
 		return
 	}
-	err = en.WriteMapHeader(uint32(len(z.Stages)))
+	err = en.WriteMapHeader(uint32(len(z.Config)))
 	if err != nil {
-		err = msgp.WrapError(err, "Stages")
+		err = msgp.WrapError(err, "Config")
 		return
 	}
-	for za0001, za0002 := range z.Stages {
+	for za0001, za0002 := range z.Config {
 		err = en.WriteString(za0001)
 		if err != nil {
-			err = msgp.WrapError(err, "Stages")
+			err = msgp.WrapError(err, "Config")
 			return
 		}
 		err = en.WriteUint8(za0002)
 		if err != nil {
-			err = msgp.WrapError(err, "Stages", za0001)
+			err = msgp.WrapError(err, "Config", za0001)
 			return
 		}
+	}
+	// write "Topic"
+	err = en.Append(0xa5, 0x54, 0x6f, 0x70, 0x69, 0x63)
+	if err != nil {
+		return
+	}
+	err = en.WriteString(z.Topic)
+	if err != nil {
+		err = msgp.WrapError(err, "Topic")
+		return
 	}
 	// write "Key"
 	err = en.Append(0xa3, 0x4b, 0x65, 0x79)
@@ -128,14 +144,14 @@ func (z *ControlMetadata) EncodeMsg(en *msgp.Writer) (err error) {
 		err = msgp.WrapError(err, "Epoch")
 		return
 	}
-	// write "InstanceId"
-	err = en.Append(0xaa, 0x49, 0x6e, 0x73, 0x74, 0x61, 0x6e, 0x63, 0x65, 0x49, 0x64)
+	// write "SubstreamId"
+	err = en.Append(0xab, 0x53, 0x75, 0x62, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x49, 0x64)
 	if err != nil {
 		return
 	}
-	err = en.WriteUint8(z.InstanceId)
+	err = en.WriteUint8(z.SubstreamId)
 	if err != nil {
-		err = msgp.WrapError(err, "InstanceId")
+		err = msgp.WrapError(err, "SubstreamId")
 		return
 	}
 	return
@@ -144,23 +160,26 @@ func (z *ControlMetadata) EncodeMsg(en *msgp.Writer) (err error) {
 // MarshalMsg implements msgp.Marshaler
 func (z *ControlMetadata) MarshalMsg(b []byte) (o []byte, err error) {
 	o = msgp.Require(b, z.Msgsize())
-	// map header, size 4
-	// string "Stages"
-	o = append(o, 0x84, 0xa6, 0x53, 0x74, 0x61, 0x67, 0x65, 0x73)
-	o = msgp.AppendMapHeader(o, uint32(len(z.Stages)))
-	for za0001, za0002 := range z.Stages {
+	// map header, size 5
+	// string "Config"
+	o = append(o, 0x85, 0xa6, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67)
+	o = msgp.AppendMapHeader(o, uint32(len(z.Config)))
+	for za0001, za0002 := range z.Config {
 		o = msgp.AppendString(o, za0001)
 		o = msgp.AppendUint8(o, za0002)
 	}
+	// string "Topic"
+	o = append(o, 0xa5, 0x54, 0x6f, 0x70, 0x69, 0x63)
+	o = msgp.AppendString(o, z.Topic)
 	// string "Key"
 	o = append(o, 0xa3, 0x4b, 0x65, 0x79)
 	o = msgp.AppendBytes(o, z.Key)
 	// string "Epoch"
 	o = append(o, 0xa5, 0x45, 0x70, 0x6f, 0x63, 0x68)
 	o = msgp.AppendUint64(o, z.Epoch)
-	// string "InstanceId"
-	o = append(o, 0xaa, 0x49, 0x6e, 0x73, 0x74, 0x61, 0x6e, 0x63, 0x65, 0x49, 0x64)
-	o = msgp.AppendUint8(o, z.InstanceId)
+	// string "SubstreamId"
+	o = append(o, 0xab, 0x53, 0x75, 0x62, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x49, 0x64)
+	o = msgp.AppendUint8(o, z.SubstreamId)
 	return
 }
 
@@ -182,18 +201,18 @@ func (z *ControlMetadata) UnmarshalMsg(bts []byte) (o []byte, err error) {
 			return
 		}
 		switch msgp.UnsafeString(field) {
-		case "Stages":
+		case "Config":
 			var zb0002 uint32
 			zb0002, bts, err = msgp.ReadMapHeaderBytes(bts)
 			if err != nil {
-				err = msgp.WrapError(err, "Stages")
+				err = msgp.WrapError(err, "Config")
 				return
 			}
-			if z.Stages == nil {
-				z.Stages = make(map[string]uint8, zb0002)
-			} else if len(z.Stages) > 0 {
-				for key := range z.Stages {
-					delete(z.Stages, key)
+			if z.Config == nil {
+				z.Config = make(map[string]uint8, zb0002)
+			} else if len(z.Config) > 0 {
+				for key := range z.Config {
+					delete(z.Config, key)
 				}
 			}
 			for zb0002 > 0 {
@@ -202,15 +221,21 @@ func (z *ControlMetadata) UnmarshalMsg(bts []byte) (o []byte, err error) {
 				zb0002--
 				za0001, bts, err = msgp.ReadStringBytes(bts)
 				if err != nil {
-					err = msgp.WrapError(err, "Stages")
+					err = msgp.WrapError(err, "Config")
 					return
 				}
 				za0002, bts, err = msgp.ReadUint8Bytes(bts)
 				if err != nil {
-					err = msgp.WrapError(err, "Stages", za0001)
+					err = msgp.WrapError(err, "Config", za0001)
 					return
 				}
-				z.Stages[za0001] = za0002
+				z.Config[za0001] = za0002
+			}
+		case "Topic":
+			z.Topic, bts, err = msgp.ReadStringBytes(bts)
+			if err != nil {
+				err = msgp.WrapError(err, "Topic")
+				return
 			}
 		case "Key":
 			z.Key, bts, err = msgp.ReadBytesBytes(bts, z.Key)
@@ -224,10 +249,10 @@ func (z *ControlMetadata) UnmarshalMsg(bts []byte) (o []byte, err error) {
 				err = msgp.WrapError(err, "Epoch")
 				return
 			}
-		case "InstanceId":
-			z.InstanceId, bts, err = msgp.ReadUint8Bytes(bts)
+		case "SubstreamId":
+			z.SubstreamId, bts, err = msgp.ReadUint8Bytes(bts)
 			if err != nil {
-				err = msgp.WrapError(err, "InstanceId")
+				err = msgp.WrapError(err, "SubstreamId")
 				return
 			}
 		default:
@@ -245,12 +270,12 @@ func (z *ControlMetadata) UnmarshalMsg(bts []byte) (o []byte, err error) {
 // Msgsize returns an upper bound estimate of the number of bytes occupied by the serialized message
 func (z *ControlMetadata) Msgsize() (s int) {
 	s = 1 + 7 + msgp.MapHeaderSize
-	if z.Stages != nil {
-		for za0001, za0002 := range z.Stages {
+	if z.Config != nil {
+		for za0001, za0002 := range z.Config {
 			_ = za0002
 			s += msgp.StringPrefixSize + len(za0001) + msgp.Uint8Size
 		}
 	}
-	s += 4 + msgp.BytesPrefixSize + len(z.Key) + 6 + msgp.Uint64Size + 11 + msgp.Uint8Size
+	s += 6 + msgp.StringPrefixSize + len(z.Topic) + 4 + msgp.BytesPrefixSize + len(z.Key) + 6 + msgp.Uint64Size + 12 + msgp.Uint8Size
 	return
 }
