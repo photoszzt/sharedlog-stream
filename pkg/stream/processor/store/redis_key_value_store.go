33f833a71e837d85c0a7823337f95db2050ef3c8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-02-27 00:01:57 -0600

    implement redis kv store
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/stream/processor/store/redis_key_value_store.go
+++ pkg/stream/processor/store/redis_key_value_store.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"sharedlog-stream/pkg/stream/processor/commtypes"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -11,6 +12,8 @@ import (
 type RedisKeyValueStore struct {
 	name string
 	rdb  *redis.Client
+	addr string
+	port uint32
 }
 
 var _ = KeyValueStore(&RedisKeyValueStore{})
@@ -24,6 +27,8 @@ func NewRedisKeyValueStore(name string, addr string, port uint32) *RedisKeyValue
 	return &RedisKeyValueStore{
 		name: name,
 		rdb:  rdb,
+		addr: addr,
+		port: port,
 	}
 }
 
@@ -73,18 +78,50 @@ func (s *RedisKeyValueStore) ApproximateNumEntries(ctx context.Context) (uint64,
 }
 
 func (s *RedisKeyValueStore) Put(ctx context.Context, key KeyT, value ValueT) error {
-
-	return nil
+	if value == nil {
+		return s.Delete(ctx, key)
+	} else {
+		// assume key and value to be bytes
+		k := key.([]byte)
+		v := value.([]byte)
+		key_str := string(k)
+		err := s.rdb.Set(ctx, key_str, v, 0).Err()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 }
 
 func (s *RedisKeyValueStore) PutIfAbsent(ctx context.Context, key KeyT, value ValueT) (ValueT, error) {
-	return nil, nil
+	// assume key and value to be bytes
+	k := key.([]byte)
+	v := value.([]byte)
+	key_str := string(k)
+	ret, err := s.rdb.SetArgs(ctx, key_str, v, redis.SetArgs{
+		Mode: "NX",
+		TTL:  time.Duration(0),
+		Get:  true,
+	}).Result()
+	if err != nil {
+		return nil, err
+	}
+	return []byte(ret), nil
 }
 
-func (s *RedisKeyValueStore) PutAll(context.Context, []*commtypes.Message) error {
+func (s *RedisKeyValueStore) PutAll(ctx context.Context, entries []*commtypes.Message) error {
+	for _, msg := range entries {
+		err := s.Put(ctx, msg.Key, msg.Value)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (s *RedisKeyValueStore) Delete(ctx context.Context, key KeyT) error {
-	return nil
+	k := key.([]byte)
+	key_str := string(k)
+	err := s.rdb.Del(ctx, key_str).Err()
+	return err
 }
