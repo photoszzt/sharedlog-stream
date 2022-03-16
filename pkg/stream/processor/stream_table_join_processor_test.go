81c7f6381738f96c5f29891027a09312ca373a48
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-03-16 13:03:56 -0500

    test stream table join
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/stream/processor/stream_table_join_processor_test.go
+++ pkg/stream/processor/stream_table_join_processor_test.go
@@ -3,6 +3,7 @@ package processor
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"sharedlog-stream/pkg/stream/processor/commtypes"
 	"sharedlog-stream/pkg/stream/processor/store"
 	"sharedlog-stream/pkg/treemap"
@@ -27,6 +28,32 @@ func getJoinProcessor() *StreamTableJoinProcessor {
 			if rightValue != nil {
 				rv := rightValue.(commtypes.ValueTimestamp)
 				return fmt.Sprintf("%d+%d", lv, rv.Value.(int))
+			} else {
+				return nil
+			}
+		},
+	))
+	return joinProc
+}
+
+func getJoinProcessorWithStr() *StreamTableJoinProcessor {
+	store := store.NewInMemoryKeyValueStore("test1", func(a, b treemap.Key) int {
+		vala := a.(int)
+		valb := b.(int)
+		if vala > valb {
+			return 1
+		} else if vala == valb {
+			return 0
+		} else {
+			return -1
+		}
+	})
+	joinProc := NewStreamTableJoinProcessor("test1", store, ValueJoinerWithKeyFunc(
+		func(readOnlyKey interface{}, leftValue interface{}, rightValue interface{}) interface{} {
+			lv := leftValue.(string)
+			if rightValue != nil {
+				rv := rightValue.(commtypes.ValueTimestamp)
+				return fmt.Sprintf("%s+%s", lv, rv.Value.(string))
 			} else {
 				return nil
 			}
@@ -61,5 +88,58 @@ func TestJoinOnlyIfMatchFound(t *testing.T) {
 				t.Error("should return no value")
 			}
 		}
+	}
+}
+
+func TestShouldClearTableEntryOnNullValueUpdate(t *testing.T) {
+	joinProc := getJoinProcessorWithStr()
+	ctx := context.Background()
+	for i := 0; i < 4; i++ {
+		err := joinProc.store.Put(ctx, i, commtypes.ValueTimestamp{Value: fmt.Sprintf("Y%d", i), Timestamp: int64(i)})
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+	got_msgs := make([]commtypes.Message, 0)
+	for i := 0; i < 4; i++ {
+		msgs, err := joinProc.ProcessAndReturn(ctx,
+			commtypes.Message{Key: i, Value: fmt.Sprintf("X%d", i), Timestamp: int64(i)})
+		if err != nil {
+			t.Errorf("fail to join: %v", err)
+		}
+		got_msgs = append(got_msgs, msgs...)
+	}
+	expected_msgs := []commtypes.Message{
+		{Key: 0, Value: "X0+Y0", Timestamp: 0},
+		{Key: 1, Value: "X1+Y1", Timestamp: 1},
+		{Key: 2, Value: "X2+Y2", Timestamp: 2},
+		{Key: 3, Value: "X3+Y3", Timestamp: 3},
+	}
+	if !reflect.DeepEqual(expected_msgs, got_msgs) {
+		t.Fatal("should equal")
+	}
+
+	for i := 0; i < 2; i++ {
+		err := joinProc.store.Put(ctx, i, nil)
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+
+	got_msgs = make([]commtypes.Message, 0)
+	for i := 0; i < 4; i++ {
+		msgs, err := joinProc.ProcessAndReturn(ctx,
+			commtypes.Message{Key: i, Value: fmt.Sprintf("XX%d", i), Timestamp: int64(i)})
+		if err != nil {
+			t.Errorf("fail to join: %v", err)
+		}
+		got_msgs = append(got_msgs, msgs...)
+	}
+	expected_msgs = []commtypes.Message{
+		{Key: 2, Value: "XX2+Y2", Timestamp: 2},
+		{Key: 3, Value: "XX3+Y3", Timestamp: 3},
+	}
+	if !reflect.DeepEqual(expected_msgs, got_msgs) {
+		t.Fatal("should equal")
 	}
 }
