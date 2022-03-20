49b32fd0269be2d516dea68f212096c844cfbb08
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-03-19 19:41:53 -0500

    test mongo db restore
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/stream/processor/store/mongodb_key_value_store_test.go
+++ pkg/stream/processor/store/mongodb_key_value_store_test.go
@@ -44,3 +44,32 @@ func TestMongoDBPutGetRange(t *testing.T) {
 	PutGetRange(ctx, store, t)
 	store.client.Database("test").Drop(ctx)
 }
+
+func TestMongoDBRestore(t *testing.T) {
+	ctx := context.Background()
+	store := getMongoDBKeyValueStore(ctx, t)
+	store.client.Database("test").Drop(ctx)
+	if err := store.Put(ctx, 0, "zero"); err != nil {
+		t.Fatal(err.Error())
+	}
+	if err := store.Put(ctx, 1, "one"); err != nil {
+		t.Fatal(err.Error())
+	}
+	if err := store.Put(ctx, 2, "two"); err != nil {
+		t.Fatal(err.Error())
+	}
+	if err := store.Put(ctx, 3, "three"); err != nil {
+		t.Fatal(err.Error())
+	}
+	store2 := getMongoDBKeyValueStore(ctx, t)
+	ret := make(map[int]string)
+	store2.Range(ctx, nil, nil, func(kt commtypes.KeyT, vt commtypes.ValueT) error {
+		ret[kt.(int)] = vt.(string)
+		return nil
+	})
+	expected := map[int]string{
+		0: "zero", 1: "one", 2: "two", 3: "three",
+	}
+	checkMapEqual(t, expected, ret)
+	store.client.Database("test").Drop(ctx)
+}
