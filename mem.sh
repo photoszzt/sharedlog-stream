79f56541b79438d0f9cddec2cc3e620796f2bf6a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-04-04 17:39:58 -0500

    cli script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ mem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+    echo "should provide app name"
+    exit 1
+fi
+
+APP_NAME=$1
+TRAN=${2:-false}
+if [ "$TRAN" = "true" ]; then
+    ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/${APP_NAME}.json \
+        --tran -comm_every_niter 100 --duration 20  -comm_everyMS 0 -tab_type mem
+else
+    ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/${APP_NAME}.json \
+        --duration 20 -tab_type mem
+fi
