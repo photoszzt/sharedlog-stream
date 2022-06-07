89d2df1615f22cd13eb567f784fa1eb7cab05e3e
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-06-07 11:07:50 -0500

    add events gen
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- benchmark/nexmark/pkg/nexmark/types/model_gen.go
+++ benchmark/nexmark/pkg/nexmark/types/model_gen.go
@@ -1549,6 +1549,152 @@ func (z EventMsgpSerde) Msgsize() (s int) {
 }
 
 // DecodeMsg implements msgp.Decodable
+func (z *Events) DecodeMsg(dc *msgp.Reader) (err error) {
+	var field []byte
+	_ = field
+	var zb0001 uint32
+	zb0001, err = dc.ReadMapHeader()
+	if err != nil {
+		err = msgp.WrapError(err)
+		return
+	}
+	for zb0001 > 0 {
+		zb0001--
+		field, err = dc.ReadMapKeyPtr()
+		if err != nil {
+			err = msgp.WrapError(err)
+			return
+		}
+		switch msgp.UnsafeString(field) {
+		case "EventsArr":
+			var zb0002 uint32
+			zb0002, err = dc.ReadArrayHeader()
+			if err != nil {
+				err = msgp.WrapError(err, "EventsArr")
+				return
+			}
+			if cap(z.EventsArr) >= int(zb0002) {
+				z.EventsArr = (z.EventsArr)[:zb0002]
+			} else {
+				z.EventsArr = make([]Event, zb0002)
+			}
+			for za0001 := range z.EventsArr {
+				err = z.EventsArr[za0001].DecodeMsg(dc)
+				if err != nil {
+					err = msgp.WrapError(err, "EventsArr", za0001)
+					return
+				}
+			}
+		default:
+			err = dc.Skip()
+			if err != nil {
+				err = msgp.WrapError(err)
+				return
+			}
+		}
+	}
+	return
+}
+
+// EncodeMsg implements msgp.Encodable
+func (z *Events) EncodeMsg(en *msgp.Writer) (err error) {
+	// map header, size 1
+	// write "EventsArr"
+	err = en.Append(0x81, 0xa9, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x73, 0x41, 0x72, 0x72)
+	if err != nil {
+		return
+	}
+	err = en.WriteArrayHeader(uint32(len(z.EventsArr)))
+	if err != nil {
+		err = msgp.WrapError(err, "EventsArr")
+		return
+	}
+	for za0001 := range z.EventsArr {
+		err = z.EventsArr[za0001].EncodeMsg(en)
+		if err != nil {
+			err = msgp.WrapError(err, "EventsArr", za0001)
+			return
+		}
+	}
+	return
+}
+
+// MarshalMsg implements msgp.Marshaler
+func (z *Events) MarshalMsg(b []byte) (o []byte, err error) {
+	o = msgp.Require(b, z.Msgsize())
+	// map header, size 1
+	// string "EventsArr"
+	o = append(o, 0x81, 0xa9, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x73, 0x41, 0x72, 0x72)
+	o = msgp.AppendArrayHeader(o, uint32(len(z.EventsArr)))
+	for za0001 := range z.EventsArr {
+		o, err = z.EventsArr[za0001].MarshalMsg(o)
+		if err != nil {
+			err = msgp.WrapError(err, "EventsArr", za0001)
+			return
+		}
+	}
+	return
+}
+
+// UnmarshalMsg implements msgp.Unmarshaler
+func (z *Events) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	var field []byte
+	_ = field
+	var zb0001 uint32
+	zb0001, bts, err = msgp.ReadMapHeaderBytes(bts)
+	if err != nil {
+		err = msgp.WrapError(err)
+		return
+	}
+	for zb0001 > 0 {
+		zb0001--
+		field, bts, err = msgp.ReadMapKeyZC(bts)
+		if err != nil {
+			err = msgp.WrapError(err)
+			return
+		}
+		switch msgp.UnsafeString(field) {
+		case "EventsArr":
+			var zb0002 uint32
+			zb0002, bts, err = msgp.ReadArrayHeaderBytes(bts)
+			if err != nil {
+				err = msgp.WrapError(err, "EventsArr")
+				return
+			}
+			if cap(z.EventsArr) >= int(zb0002) {
+				z.EventsArr = (z.EventsArr)[:zb0002]
+			} else {
+				z.EventsArr = make([]Event, zb0002)
+			}
+			for za0001 := range z.EventsArr {
+				bts, err = z.EventsArr[za0001].UnmarshalMsg(bts)
+				if err != nil {
+					err = msgp.WrapError(err, "EventsArr", za0001)
+					return
+				}
+			}
+		default:
+			bts, err = msgp.Skip(bts)
+			if err != nil {
+				err = msgp.WrapError(err)
+				return
+			}
+		}
+	}
+	o = bts
+	return
+}
+
+// Msgsize returns an upper bound estimate of the number of bytes occupied by the serialized message
+func (z *Events) Msgsize() (s int) {
+	s = 1 + 10 + msgp.ArrayHeaderSize
+	for za0001 := range z.EventsArr {
+		s += z.EventsArr[za0001].Msgsize()
+	}
+	return
+}
+
+// DecodeMsg implements msgp.Decodable
 func (z *Person) DecodeMsg(dc *msgp.Reader) (err error) {
 	var field []byte
 	_ = field
