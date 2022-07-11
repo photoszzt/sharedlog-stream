c49a6b079a2144c1a3161cde59521ad2e4cc2a36
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-07-11 15:50:35 -0500

    remove commit every niter
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- test.sh
+++ test.sh
@@ -17,5 +17,5 @@ DUMP_DIR=$(realpath ./dump/)
 #DUMP_DIR=""
 DUMP_DIR=${DUMP_DIR}/${APP_NAME}_$GUA
 ./bin/nexmark_client -app_name $APP_NAME -wconfig ./test_workload_config/${APP_NAME}.json \
-    -guarantee $GUA -comm_every_niter 0 -duration 10 -comm_everyMS 5 -flushms 5 -tab_type mem -serde json \
+    -guarantee $GUA -duration 130 -comm_everyMS 5 -flushms 5 -tab_type mem -serde msgp \
     -stat_dir ./${APP_NAME}_stats -testsrc ${TEST_SRC} -dumpdir ${DUMP_DIR} -local true 
