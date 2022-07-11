f094ee15bbf5f2faa2558816978f2d416c0d48c5
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-07-11 15:50:58 -0500

    remove tran
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- mem.sh
+++ mem.sh
@@ -5,14 +5,13 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-APP_NAME=$1
-TRAN=${2:-false}
-if [ "$TRAN" = "true" ]; then
-    ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/${APP_NAME}.json \
-        -tran -comm_every_niter 0 -duration 35 -comm_everyMS 100 -tab_type mem -serde msgp \
-        -stat_dir ./${APP_NAME}_stats -tps 10000 -events_num 350000 -warmup_time 15 -warmup_events 150000 -local true
-else
-    ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/${APP_NAME}.json \
-        -duration 35 -tab_type mem -serde msgp -stat_dir ./${APP_NAME}_stats \
-        -tps 10000 -events_num 350000 -warmup_time 15 -warmup_events 150000 -local true
+if [ "$2" = "" ]; then
+    echo "should provide guarantee name: <one of alo, 2pc and epoch>"
+    exit
 fi
+
+APP_NAME=$1
+GUA=${2:-alo}
+./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/4_ins/${APP_NAME}.json \
+    -guarantee $GUA -duration 60 -comm_everyMS 5 -flushms 5 -tab_type mem -serde json \
+    -stat_dir ./${APP_NAME}_stats -tps 4000 -events_num 120000 -local true 
