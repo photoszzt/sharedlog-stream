9d134a69d991a4fd3fcb93fc8b4c6bc6d74d9149
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-08-19 16:40:51 -0500

    add mem hash
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/hashfuncs/hash_string.go
+++ pkg/hashfuncs/hash_string.go
@@ -2,10 +2,36 @@ package hashfuncs
 
 import (
 	"hash/fnv"
+	"unsafe"
 )
 
 func NameHash(name string) uint64 {
 	h := fnv.New64a()
 	h.Write([]byte(name))
 	return h.Sum64()
+}
+
+type stringStruct struct {
+	str unsafe.Pointer
+	len int
+}
+
+//go:noescape
+//go:linkname memhash runtime.memhash
+func memhash(p unsafe.Pointer, h, s uintptr) uintptr
+
+// MemHash is the hash function used by go map, it utilizes available hardware instructions(behaves
+// as aeshash if aes instruction is available).
+// NOTE: The hash seed changes for every process. So, this cannot be used as a persistent hash.
+func MemHash(data []byte) uint64 {
+	ss := (*stringStruct)(unsafe.Pointer(&data))
+	return uint64(memhash(ss.str, 0, uintptr(ss.len)))
+}
+
+// MemHashString is the hash function used by go map, it utilizes available hardware instructions
+// (behaves as aeshash if aes instruction is available).
+// NOTE: The hash seed changes for every process. So, this cannot be used as a persistent hash.
+func MemHashString(str string) uint64 {
+	ss := (*stringStruct)(unsafe.Pointer(&str))
+	return uint64(memhash(ss.str, 0, uintptr(ss.len)))
 }
