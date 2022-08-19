e5f49459e04dfdd6095b160f44e28f64085dce76
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-08-19 10:07:28 -0500

    test generic version of stream table join
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/processor/stream_table_join_processor_test.go
+++ pkg/processor/stream_table_join_processor_test.go
@@ -7,6 +7,8 @@ import (
 	"sharedlog-stream/pkg/commtypes"
 	"sharedlog-stream/pkg/store"
 	"testing"
+
+	"4d63.com/optional"
 )
 
 func getJoinProcessor() *StreamTableJoinProcessor {
@@ -20,6 +22,16 @@ func getJoinProcessor() *StreamTableJoinProcessor {
 			} else {
 				return nil
 			}
+		},
+	))
+	return joinProc
+}
+
+func getSkipMapJoinProcessor() *StreamTableJoinProcessorG[int, int, int, string] {
+	store := store.NewInMemorySkipmapKeyValueStoreG[int, *commtypes.ValueTimestamp]("test1", store.IntLessFunc)
+	joinProc := NewStreamTableJoinProcessorG[int, int, int, string](store, ValueJoinerWithKeyFuncG[int, int, int, string](
+		func(readOnlyKey int, leftValue int, rightValue int) string {
+			return fmt.Sprintf("%d+%d", leftValue, rightValue)
 		},
 	))
 	return joinProc
@@ -41,15 +53,41 @@ func getJoinProcessorWithStr() *StreamTableJoinProcessor {
 	return joinProc
 }
 
+func getSkipMapJoinProcessorWithStr() *StreamTableJoinProcessorG[int, string, string, string] {
+	store := store.NewInMemorySkipmapKeyValueStoreG[int, *commtypes.ValueTimestamp]("test1", store.IntLessFunc)
+	joinProc := NewStreamTableJoinProcessorG[int, string, string, string](store, ValueJoinerWithKeyFuncG[int, string, string, string](
+		func(readOnlyKey int, leftValue string, rightValue string) string {
+			return fmt.Sprintf("%s+%s", leftValue, rightValue)
+		},
+	))
+	return joinProc
+}
+
 func TestJoinOnlyIfMatchFound(t *testing.T) {
-	joinProc := getJoinProcessor()
 	ctx := context.Background()
+	joinProc := getJoinProcessor()
 	for i := 0; i < 2; i++ {
 		err := joinProc.store.Put(ctx, i, commtypes.CreateValueTimestamp(i, int64(i)))
 		if err != nil {
 			t.Errorf("fail to put val to store: %v", err)
 		}
 	}
+	JoinOnlyIfMatchFound(t, ctx, joinProc)
+}
+
+func TestSkipMapJoinOnlyIfMatchFound(t *testing.T) {
+	ctx := context.Background()
+	joinProc := getSkipMapJoinProcessor()
+	for i := 0; i < 2; i++ {
+		err := joinProc.store.Put(ctx, i, commtypes.CreateValueTimestampOptional(optional.Of(i), int64(i)))
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+	JoinOnlyIfMatchFound(t, ctx, joinProc)
+}
+
+func JoinOnlyIfMatchFound(t *testing.T, ctx context.Context, joinProc Processor) {
 	for i := 0; i < 4; i++ {
 		msgs, err := joinProc.ProcessAndReturn(ctx, commtypes.Message{Key: i, Value: i, Timestamp: int64(i)})
 		if err != nil {
@@ -79,6 +117,44 @@ func TestShouldClearTableEntryOnNullValueUpdate(t *testing.T) {
 			t.Errorf("fail to put val to store: %v", err)
 		}
 	}
+	ShouldClearTableEntryOnNullValueUpdatePart1(t, ctx, joinProc)
+	putSecond(t, ctx, joinProc)
+	ShouldClearTableEntryOnNullValueUpdatePart2(t, ctx, joinProc)
+}
+
+func TestSkipMapShouldClearTableEntryOnNullValueUpdate(t *testing.T) {
+	joinProc := getSkipMapJoinProcessorWithStr()
+	ctx := context.Background()
+	for i := 0; i < 4; i++ {
+		err := joinProc.store.Put(ctx, i, commtypes.CreateValueTimestampOptional(optional.Of(fmt.Sprintf("Y%d", i)), int64(i)))
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+	ShouldClearTableEntryOnNullValueUpdatePart1(t, ctx, joinProc)
+	putSecondG(t, ctx, joinProc)
+	ShouldClearTableEntryOnNullValueUpdatePart2(t, ctx, joinProc)
+}
+
+func putSecond(t *testing.T, ctx context.Context, joinProc *StreamTableJoinProcessor) {
+	for i := 0; i < 2; i++ {
+		err := joinProc.store.Put(ctx, i, nil)
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+}
+
+func putSecondG(t *testing.T, ctx context.Context, joinProc *StreamTableJoinProcessorG[int, string, string, string]) {
+	for i := 0; i < 2; i++ {
+		err := joinProc.store.Put(ctx, i, optional.Empty[*commtypes.ValueTimestamp]())
+		if err != nil {
+			t.Errorf("fail to put val to store: %v", err)
+		}
+	}
+}
+
+func ShouldClearTableEntryOnNullValueUpdatePart1(t *testing.T, ctx context.Context, joinProc Processor) {
 	got_msgs := make([]commtypes.Message, 0)
 	for i := 0; i < 4; i++ {
 		msgs, err := joinProc.ProcessAndReturn(ctx,
@@ -97,15 +173,10 @@ func TestShouldClearTableEntryOnNullValueUpdate(t *testing.T) {
 	if !reflect.DeepEqual(expected_msgs, got_msgs) {
 		t.Fatal("should equal")
 	}
+}
 
-	for i := 0; i < 2; i++ {
-		err := joinProc.store.Put(ctx, i, nil)
-		if err != nil {
-			t.Errorf("fail to put val to store: %v", err)
-		}
-	}
-
-	got_msgs = make([]commtypes.Message, 0)
+func ShouldClearTableEntryOnNullValueUpdatePart2(t *testing.T, ctx context.Context, joinProc Processor) {
+	got_msgs := make([]commtypes.Message, 0)
 	for i := 0; i < 4; i++ {
 		msgs, err := joinProc.ProcessAndReturn(ctx,
 			commtypes.Message{Key: i, Value: fmt.Sprintf("XX%d", i), Timestamp: int64(i)})
@@ -114,7 +185,7 @@ func TestShouldClearTableEntryOnNullValueUpdate(t *testing.T) {
 		}
 		got_msgs = append(got_msgs, msgs...)
 	}
-	expected_msgs = []commtypes.Message{
+	expected_msgs := []commtypes.Message{
 		{Key: 2, Value: "XX2+Y2", Timestamp: 2},
 		{Key: 3, Value: "XX3+Y3", Timestamp: 3},
 	}
