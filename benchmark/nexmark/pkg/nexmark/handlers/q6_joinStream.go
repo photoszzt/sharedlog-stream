359362e17d639840a52124fc75bfdcc0d129c334
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-08-24 00:50:41 -0500

    q4 and 6 to 10 ms for consume timeout
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- benchmark/nexmark/pkg/nexmark/handlers/q6_joinStream.go
+++ benchmark/nexmark/pkg/nexmark/handlers/q6_joinStream.go
@@ -62,7 +62,7 @@ func (h *q6JoinStreamHandler) getSrcSink(ctx context.Context, sp *common.QueryIn
 	if err != nil {
 		return nil, nil, fmt.Errorf("get msg serde err: %v", err)
 	}
-	timeout := time.Duration(4) * time.Millisecond
+	timeout := time.Duration(10) * time.Millisecond
 	src1Config := &producer_consumer.StreamConsumerConfigG[uint64, *ntypes.Event]{
 		Timeout:     timeout,
 		MsgSerde:    msgSerde,
