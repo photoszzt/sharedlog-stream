3f1380f0b99475991affb0c953fb17c90257944e
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-09-06 18:23:26 -0500

    add snapshot
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- failure.sh
+++ failure.sh
@@ -13,6 +13,6 @@ fi
 APP_NAME=$1
 GUA=${2:-alo}
 ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/1_ins/${APP_NAME}.json \
-    -guarantee $GUA -duration 90 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
-    -stat_dir ./${APP_NAME}_stats -tps 1000 -events_num 90000 -waitForLast=true -local=true \
-    -fail_spec ./workload_config/1_ins/failure_config/${APP_NAME}.json
+    -guarantee $GUA -duration 60 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
+    -stat_dir ./${APP_NAME}_stats -tps 10000 -events_num 600000 -waitForLast=true -local=true \
+    -fail_spec ./workload_config/1_ins/failure_config/${APP_NAME}.json -snapshot_everyS=10
