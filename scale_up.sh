88b8d94fc7bc1c7d0a907d999bd6acec5577118a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-09-08 01:50:17 -0500

    backup 1 to 2 scripts
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- scale_up.sh
+++ scale_up.sh
@@ -12,7 +12,7 @@ fi
 
 APP_NAME=$1
 GUA=${2:-alo}
-./bin/nexmark_scale -app_name $APP_NAME -wconfig ./workload_config/4_ins/${APP_NAME}.json \
-    -scconfig ./scale_to_src_unchanged/4_to_8_ins/${APP_NAME}.json \
-    -guarantee $GUA -durBF 30 -durAF 30 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
-    -stat_dir ./${APP_NAME}_stats -tps 400000 -waitForLast=true -local=true
+./bin/nexmark_scale -app_name $APP_NAME -wconfig ./workload_config/1_ins/${APP_NAME}.json \
+    -scconfig ./scale_to_src_unchanged/1_to_2_ins/${APP_NAME}.json \
+    -guarantee $GUA -durBF 30 -durAF 60 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
+    -stat_dir ./${APP_NAME}_stats -tps 1000 -waitForLast=true -local=true
