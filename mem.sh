d759afed50a46d242f8ed1055e64897723899cef
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-09-27 20:04:26 -0500

    backup local script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- mem.sh
+++ mem.sh
@@ -6,12 +6,12 @@ if [ "$1" = "" ]; then
 fi
 
 if [ "$2" = "" ]; then
-    echo "should provide guarantee name: <one of alo, 2pc and epoch>"
+    echo "should provide guarantee name: <one of none, alo, 2pc and epoch>"
     exit
 fi
 
 APP_NAME=$1
 GUA=${2:-alo}
 ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/4_ins/${APP_NAME}.json \
-    -guarantee $GUA -duration 30 -comm_everyMS 100 -flushms 100 -tab_type mem -serde msgp \
-    -stat_dir ./${APP_NAME}_stats -tps 100 -events_num 3000 -waitForLast=true -local=true
+    -guarantee $GUA -duration 30 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
+    -stat_dir ./${APP_NAME}_stats -tps 1000 -events_num 30000 -waitForLast=true -local=true
--- mem_1ins.sh
+++ mem_1ins.sh
@@ -13,5 +13,5 @@ fi
 APP_NAME=$1
 GUA=${2:-alo}
 ./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/1_ins/${APP_NAME}.json \
-    -guarantee $GUA -duration 60 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
-    -stat_dir ./${APP_NAME}_stats -tps 10000 -events_num 600000 -waitForLast=true -local=true
+    -guarantee $GUA -duration 30 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
+    -stat_dir ./${APP_NAME}_stats -tps 10 -events_num 300 -waitForLast=true -local=true
