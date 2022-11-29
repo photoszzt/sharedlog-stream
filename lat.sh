cadd4c94a1f65eb13db32ef85f7953a89d8c27f8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-11-29 13:03:25 -0600

    local lat test
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ lat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./bin/sharedlog_protocol_lat \
+    -payload /mnt/data/src/stream_workspace/sharedlog-stream/data/payload-16Kb.data \
+    -events_num 300 -duration 30 -tps 10 -comm_everyMS 10 -local=true
