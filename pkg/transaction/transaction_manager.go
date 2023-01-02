445d76d4bfa003302326ddbd42a31105bf993ead
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-01-02 17:53:06 -0600

    add debug print
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/transaction/transaction_manager.go
+++ pkg/transaction/transaction_manager.go
@@ -449,15 +449,15 @@ func (tc *TransactionManager) FindLastConsumedSeqNum(ctx context.Context, topicT
 
 func (tc *TransactionManager) EnsurePrevTxnFinAndAppendMeta(ctx context.Context) error {
 	if env_config.ASYNC_SECOND_PHASE && atomic.LoadUint32(&tc.hasWaitForLastTxn) == 0 {
-		// debug.Fprintf(os.Stderr, "waiting for previous txn to finish\n")
+		fmt.Fprintf(os.Stderr, "waiting for previous txn to finish\n")
 		err := tc.bgErrg.Wait()
 		if err != nil {
 			return err
 		}
-		// debug.Fprintf(os.Stderr, "previous txn finished\n")
+		fmt.Fprintf(os.Stderr, "previous txn finished\n")
 	}
 	if tc.addedNewTpPar {
-		debug.Fprintf(os.Stderr, "appending tp par to txn log\n")
+		// debug.Fprintf(os.Stderr, "appending tp par to txn log\n")
 		tps := make([]txn_data.TopicPartition, 0, tc.currentTopicSubstream.Len())
 		tc.currentTopicSubstream.Range(func(key string, value *skipset.Uint32Set) bool {
 			pars := make([]uint8, 0, value.Len())
