e5c473971df5665e7b7986136529b8deb622570a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-05-24 01:04:20 -0500

    fix failure script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- failure.sh
+++ failure.sh
@@ -12,7 +12,7 @@ fi
 
 APP_NAME=$1
 GUA=${2:-alo}
-./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/1_ins/${APP_NAME}.json \
+./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/1node/1_ins/${APP_NAME}.json \
     -guarantee $GUA -duration 60 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
     -stat_dir ./${APP_NAME}_stats -tps 10000 -events_num 600000 -waitForLast=true -local=true \
-    -fail_spec ./workload_config/1_ins/failure_config/${APP_NAME}.json -snapshot_everyS=10
+    -fail_spec ./workload_config/1node/1_ins/failure_config/${APP_NAME}.json -snapshot_everyS=10
