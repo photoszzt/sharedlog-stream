63784e154c8a0da6386c5faa7a5f615053ef9a9a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-01-21 19:07:49 -0600

    fix path
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- mem.sh
+++ mem.sh
@@ -6,12 +6,12 @@ if [ "$1" = "" ]; then
 fi
 
 if [ "$2" = "" ]; then
-    echo "should provide guarantee name: <one of none, alo, 2pc and epoch>"
+    echo "should provide guarantee name: <one of none, alo, 2pc, align_chkpt and epoch>"
     exit
 fi
 
 APP_NAME=$1
 GUA=${2:-alo}
-./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/4_ins/${APP_NAME}.json \
+./bin/nexmark_client -app_name $APP_NAME -wconfig ./workload_config/4node/2_ins/${APP_NAME}.json \
     -guarantee $GUA -duration 30 -comm_everyMS 100 -src_flushms 100 -flushms 100 -serde msgp \
     -stat_dir ./${APP_NAME}_stats -tps 1000 -events_num 30000 -waitForLast=true -local=true
