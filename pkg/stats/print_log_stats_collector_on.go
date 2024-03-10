cda23bfaef62f2f9f1f988af2e255fe8891aa250
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-10 01:25:41 -0600

    use same format
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pkg/stats/print_log_stats_collector_on.go
+++ pkg/stats/print_log_stats_collector_on.go
@@ -10,7 +10,7 @@ import (
 
 func (c *PrintLogStatsCollector[E]) AddSample(sample E) {
 	if len(c.data) >= cap(c.data) {
-		fmt.Fprintf(os.Stderr, "%s (%d samples): %v\n", c.tag, len(c.data), c.data)
+		fmt.Fprintf(os.Stderr, "%s (%d samples): data=%v\n", c.tag, len(c.data), c.data)
 		c.data = make([]E, 0, cap(c.data))
 	}
 	c.data = append(c.data, sample)
