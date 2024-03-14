82b3c135e2e3f9f9dffe4fc9c3730282e41d4cbf
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-13 23:31:34 -0500

    extract cap and length

--- pkg/stats/print_log_stats_collector_on.go
+++ pkg/stats/print_log_stats_collector_on.go
@@ -9,9 +9,11 @@ import (
 )
 
 func (c *PrintLogStatsCollector[E]) AddSample(sample E) {
-	if len(c.data) >= cap(c.data) {
-		fmt.Fprintf(os.Stderr, "%s (%d samples): data=%v\n", c.tag, len(c.data), c.data)
-		c.data = make([]E, 0, cap(c.data))
+	l_d := len(c.data)
+	cap_d := cap(c.data)
+	if l_d >= cap_d {
+		fmt.Fprintf(os.Stderr, "%s (%d samples): data=%v\n", c.tag, l_d, c.data)
+		c.data = make([]E, 0, cap_d)
 	}
 	c.data = append(c.data, sample)
 }
