375c3621822721f2d38d4774c9bf0e219944cb9a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-05-03 04:49:02 +0000

    launch the server when creating the func factory

--- remote_txn_manager/remote_txn_manager.go
+++ remote_txn_manager/remote_txn_manager.go
@@ -11,9 +11,8 @@ import (
 	"sharedlog-stream/pkg/transaction/remote_txn_rpc"
 	"strconv"
 
-	ipc "cs.utexas.edu/zjia/faas/ipc"
+	"cs.utexas.edu/zjia/faas"
 	"cs.utexas.edu/zjia/faas/types"
-	"cs.utexas.edu/zjia/faas/worker"
 	"google.golang.org/grpc"
 )
 
@@ -30,14 +29,6 @@ func (h *emptyFuncHanlder) Call(ctx context.Context, input []byte) ([]byte, erro
 }
 
 func (f *emptyFuncHandlerFactory) New(env types.Environment, funcName string) (types.FuncHandler, error) {
-	return &emptyFuncHanlder{}, nil
-}
-
-func (f *emptyFuncHandlerFactory) GrpcNew(env types.Environment, service string) (types.GrpcFuncHandler, error) {
-	return nil, fmt.Errorf("not implemented")
-}
-
-func main() {
 	var port int
 	var err error
 	serde := os.Getenv("RTX_SERDE_FORMAT")
@@ -47,7 +38,7 @@ func main() {
 	}
 	portStr := os.Getenv("RTX_PORT")
 	if portStr == "" {
-		port = 50051
+		port = 50052
 	} else {
 		port, err = strconv.Atoi(portStr)
 		if err != nil {
@@ -55,31 +46,26 @@ func main() {
 		}
 	}
 	serdeFormat := common.GetSerdeFormat(serde)
-	log.Printf("[INFO] port %v, serdeFormat %v(%v)\n", port, serde, serdeFormat)
-	ipc.SetRootPathForIpc(os.Getenv("FAAS_ROOT_PATH_FOR_IPC"))
-	funcId, err := strconv.Atoi(os.Getenv("FAAS_FUNC_ID"))
-	if err != nil {
-		log.Fatal("[FATAL] Failed to parse FAAS_FUNC_ID")
-	}
-	clientId, err := strconv.Atoi(os.Getenv("FAAS_CLIENT_ID"))
-	if err != nil {
-		log.Fatal("[FATAL] Failed to parse FAAS_CLIENT_ID")
-	}
-	w, err := worker.NewFuncWorker(uint16(funcId), uint16(clientId), &emptyFuncHandlerFactory{})
-	if err != nil {
-		log.Fatal("[FATAL] Failed to create FuncWorker: ", err)
-	}
-	go func(w *worker.FuncWorker) {
-		w.Run()
-	}(w)
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	log.Printf("[INFO] port %v, serdeFormat %v\n", port, serdeFormat)
+	lis, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen %v: %v", port, err)
 	}
 	grpcServer := grpc.NewServer()
-	remote_txn_rpc.RegisterRemoteTxnMngrServer(grpcServer, transaction.NewRemoteTxnManager(w, serdeFormat))
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		log.Fatalf("failed to serve grpc server: %v", err)
-	}
+	remote_txn_rpc.RegisterRemoteTxnMngrServer(grpcServer, transaction.NewRemoteTxnManager(env, serdeFormat))
+	go func() {
+		err = grpcServer.Serve(lis)
+		if err != nil {
+			log.Fatalf("failed to serve grpc server: %v", err)
+		}
+	}()
+	return &emptyFuncHanlder{}, nil
+}
+
+func (f *emptyFuncHandlerFactory) GrpcNew(env types.Environment, service string) (types.GrpcFuncHandler, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func main() {
+	faas.Serve(&emptyFuncHandlerFactory{})
 }
