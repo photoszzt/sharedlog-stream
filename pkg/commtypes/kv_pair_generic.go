eafb8c15480e4443be3e699f3730853d4d0a4173
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-05-15 12:10:01 -0500

    need to check whether the buffer is nil

--- pkg/commtypes/kv_pair_generic.go
+++ pkg/commtypes/kv_pair_generic.go
@@ -94,11 +94,11 @@ func (s KeyValuePairMsgpSerdeG[K, V]) String() string {
 func (s KeyValuePairMsgpSerdeG[K, V]) Encode(v *KeyValuePair[K, V]) ([]byte, *[]byte, error) {
 	kvser, kbuf, vbuf, err := KVPairToKVPairSer(v, s.keySerde, s.valSerde)
 	defer func() {
-		if s.keySerde.UsedBufferPool() && kvser.KeyEnc != nil {
+		if s.keySerde.UsedBufferPool() && kbuf != nil {
 			*kbuf = kvser.KeyEnc
 			PushBuffer(kbuf)
 		}
-		if s.valSerde.UsedBufferPool() && kvser.ValueEnc != nil {
+		if s.valSerde.UsedBufferPool() && vbuf != nil {
 			*vbuf = kvser.ValueEnc
 			PushBuffer(vbuf)
 		}
